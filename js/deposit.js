bb1cd4096809af217998c66b6b2a7c221259bfa0
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-08-17 14:37:59 +0600

    correction of deposit.js

--- js/deposit.js
+++ js/deposit.js
@@ -8,6 +8,14 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const newDepositAmount = parseFloat(newDepositAmountString);
     // console.log(depositAmount);
 
+    // step 7: clear the deposit field
+    depositField.value = '';
+
+    if(isNaN(newDepositAmount)){
+        alert('Please enter a valid amount');
+        return;
+    }
+
     // step 3: get the current deposit total
     //for non input (element other than input, textarea) use innerText to get the text.
     const depositTotalElement = document.getElementById('deposit-total');
@@ -24,11 +32,35 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const currentTotalBalanceString = previousTotalBalance.innerText;
     const currentTotalBalance = parseFloat(currentTotalBalanceString);
     // step:6 calculate current total balance
-    const newTotalBalance = currentDepositTotal + currentTotalBalance;
+    const newTotalBalance = newDepositAmount + currentTotalBalance;
     // set the deposit total
     previousTotalBalance.innerText = newTotalBalance;
+}) 
 
-    // step 7: clear the deposit field
+/* document.getElementById('btn-deposit').addEventListener('click', function(){
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
     depositField.value = '';
 
-})
+    if(isNaN(newDepositAmount)){
+        alert('Please enter a valid amount');
+        return;
+    }
+
+    const previousDeposit = document.getElementById('deposit-total');
+    const previousDepositTotalString = previousDeposit.innerText;
+    const previousTotalDeposit = parseFloat(previousDepositTotalString);
+
+    const newTotalDeposit = previousTotalDeposit + newDepositAmount;
+    previousDeposit.innerText = newTotalDeposit;
+
+
+    const previousTotal = document.getElementById('total-balance');
+    const previousTotalBalanceString = previousTotal.innerText;
+    const previousTotalBalance = parseFloat(previousTotalBalanceString);
+
+    const newTotalBalance = previousTotalBalance + newDepositAmount;
+    previousTotal.innerText = newTotalBalance;
+});  */
